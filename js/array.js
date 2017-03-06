7b73f76e82e3548ef7d3b6309214e7d1e37453b2
Alex Jimmy <alu0100728170@ull.edu.es>
2017-03-06 01:42:16 +0000

    Mejoras en el diseño y animacion de los elementos del vector S. Implementada variable booleana 'run_animation'

--- js/array.js
+++ js/array.js
@@ -4,7 +4,8 @@ var vectorEstado = [];
 var vectorClave = [];
 var index_i=0;
 var index_f=0;
-var elemento_f=$('#valor-f');
+var elemento_f=$('.elemento.f');
+var run_animation = false;
 //Generar Vector Estado
 (function (){
     var vector = $('.vector-estado');
@@ -13,15 +14,17 @@ var elemento_f=$('#valor-f');
         for(var j=0; j<16; j++){
             var value = i*16+j;
             fila += `
-                    <div class="elemento" data-value="${value}">
-                        ${value}
+                    <div class="elemento">
+                        <div class="value" data-value="${value}">
+                            ${value}
+                        </div>
                         <span class="index">${value}</span>
                     </div>`;
         }
         fila+='</div>'
         vector.append(fila);
     }
-    $('.vector-estado .elemento').each(function(){
+    $('.vector-estado .elemento .value').each(function(){
         vectorEstado.push($(this));
     });
 
@@ -32,16 +35,19 @@ function generateKeyVector(clave){
         var fila = '<div class="fila">';
         for(var j=0; j<16; j++){
             var index = (i*16+j);
+            var value = clave[index%clave.length];
             fila += `
-                    <div class="elemento" data-value="${clave[index%clave.length]}">
-                        ${clave[index%clave.length]}
+                    <div class="elemento">
+                        <div class="value" data-value="${value}">
+                        ${value}
+                        </div>
                         <span class="index">${index}</span>
                     </div>`;
         }
         fila+='</div>'
         vector.append(fila);
     }
-    $('.vector-clave .elemento').each(function(){
+    $('.vector-clave .elemento .value').each(function(){
         vectorClave.push($(this));
     });
 }
@@ -57,13 +63,13 @@ function swap(i,j, callback){
         .animate({
             left: `+=${coor_j.left - coor_i.left}`,
             top: `+=${coor_j.top - coor_i.top}`
-        },2000);
+        },1000);
     elemento_j
         .css({'border-color': 'green','z-index':'10'})
         .animate({
             left: `+=${coor_i.left - coor_j.left}`,
             top: `+=${coor_i.top - coor_j.top}`
-        },2000, function(){
+        },1000, function(){
             callback(i,j);
         });;
 }
@@ -86,8 +92,6 @@ function swapStepVector(){
         index_f %= 256;
         elemento_f.text(index_f);
         swap(index_i,index_f, function(i,f){
-            vectorEstado[i].find('span').text(f);
-            vectorEstado[f].find('span').text(i);
             resetStyles(i,f);
             var tmp = vectorEstado[i];
             vectorEstado[i] = vectorEstado[f];
@@ -95,6 +99,7 @@ function swapStepVector(){
             if(++i<=256){
                 selectElement(i);
             }
+            run_animation=false;
         });
         console.log(index_i,index_f);
         index_i++;
@@ -120,17 +125,22 @@ function swapCompleteVector(){
     }
     for(var i=0; i<256; i++){
         vectorEstado[i].attr('data-value',vectorS[i]);
-        vectorEstado[i][0].childNodes[0].nodeValue=vectorS[i];
+        vectorEstado[i].text(vectorS[i]);
 
     }
 }
 $('button#step').on('click',function(){
-    swapStepVector();
+    if(!run_animation) {
+        run_animation=true;
+        swapStepVector();
+    }
 });
 $('button#ir-final').on('click',function(){
-    resetStyles(index_i,index_f);
-    elemento_f.text('');
-    swapCompleteVector();
+    if(!run_animation){
+        resetStyles(index_i,index_f);
+        elemento_f.text('');
+        swapCompleteVector();
+    }
 
 })
 function init(){
